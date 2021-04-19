9e0f59adaabe6818c73a2c50404702b7992c3f90
wenbo <wenmobo2018@gmai.com>
2021-04-19 22:46:05 +0800

    环形链表

--- LeetCode/Java/src/链表/剑指Offer24反转链表.java
+++ LeetCode/Java/src/链表/剑指Offer24反转链表.java
@@ -1,16 +1,32 @@
 package 链表;
 
+import jdk.javadoc.internal.doclets.formats.html.markup.Head;
+
 public class 剑指Offer24反转链表 {
 
 	class Solution {
 		/// 递归实现
 	    public ListNode reverseList(ListNode head) {
-	    	if ( head == null ||head.next == null) return head;
+	    	if ( head == null || head.next == null) return head;
 	    	ListNode newHead = reverseList(head.next);
 	    	head.next.next = head;
 	    	head.next = null;
 	    	return newHead;
 	    }
+	    
+	    /// 非递归
+	    public ListNode reverseList2(ListNode head) {
+	    	if ( head == null || head.next == null) return head;
+	    	
+	    	ListNode newHead = null;
+	    	while (head != null) {
+				ListNode temp = head.next;
+				head.next = newHead;
+				newHead = head;
+				head = temp;
+			}
+	    	return newHead;
+		}
 	}
 	
 }
