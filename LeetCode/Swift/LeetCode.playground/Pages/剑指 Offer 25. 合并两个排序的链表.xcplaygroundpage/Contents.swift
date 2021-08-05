26407621e15d797ddfe6375dadcca2422a40dbd5
wenbo <wenmobo2018@gmai.com>
2021-08-05 18:47:41 +0800

    添加递归实现

--- LeetCode/Swift/LeetCode.playground/Pages/剑指 Offer 25. 合并两个排序的链表.xcplaygroundpage/Contents.swift
+++ LeetCode/Swift/LeetCode.playground/Pages/剑指 Offer 25. 合并两个排序的链表.xcplaygroundpage/Contents.swift
@@ -16,26 +16,49 @@ public class ListNode {
 ///   - l1: 链表1
 ///   - l2: 链表2
 /// - Returns: 合并之后的链表
+//func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+//    let dum = ListNode(-1)
+//    var cur: ListNode? = dum
+//
+//    var p1: ListNode? = l1
+//    var p2: ListNode? = l2
+//    while p1 != nil && p2 != nil {
+//        if p1!.val < p2!.val {
+//            cur?.next = p1
+//            p1 = p1?.next
+//        } else {
+//            cur?.next = p2
+//            p2 = p2?.next
+//        }
+//        cur = cur?.next
+//    }
+//
+//    cur?.next = p1 ?? p2
+//
+//    return dum.next
+//}
+
+
+/// 递归实现
+/// - Parameters:
+///   - l1: 链表1
+///   - l2: 链表2
+/// - Returns: 合并之后的链表
 func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-    let dum = ListNode(-1)
-    var cur: ListNode? = dum
-    
-    var p1: ListNode? = l1
-    var p2: ListNode? = l2
-    while p1 != nil && p2 != nil {
-        if p1!.val < p2!.val {
-            cur?.next = p1
-            p1 = p1?.next
-        } else {
-            cur?.next = p2
-            p2 = p2?.next
-        }
-        cur = cur?.next
+    if l1 == nil {
+        return l2
+    }
+    if l2 == nil {
+        return l1
     }
     
-    cur?.next = p1 ?? p2
-    
-    return dum.next
+    if l1!.val < l2!.val {
+        l1?.next = mergeTwoLists(l1?.next, l2)
+        return l1
+    } else {
+        l2?.next = mergeTwoLists(l1, l2?.next)
+        return l2
+    }
 }
 
 //: [Next](@next)
