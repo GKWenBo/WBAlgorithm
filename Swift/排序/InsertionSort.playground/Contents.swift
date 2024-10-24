0f202898cced3c6d224d2eb7895622802b5db93a
wenbo <wenmobo2018@gmai.com>
2024-10-24 23:07:55 +0800

    add 插入排序

--- Swift/排序/InsertionSort.playground/Contents.swift
+++ Swift/排序/InsertionSort.playground/Contents.swift
@@ -1,9 +1,7 @@
-//: [Previous](@previous)
-
 import Foundation
 
 /*
- 插入排序
+ 插入排序 时间复杂度：O(N²)
  > - 将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
  > - 从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。（如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
  */
@@ -24,9 +22,25 @@ func insertionSort(array: inout Array<Int>) {
     }
 }
 
-/// test
+func insertionSort1(array: inout Array<Int>) {
+    guard array.count > 1 else { return }
+    // 0 ~ 0 有序的
+    // 0 ~ i 想有序
+    for i in 1..<array.count {
+        var j = i - 1
+        while j >= 0 && array[j] > array[j + 1] {
+            array.swapAt(j, j + 1)
+            j -= 1
+        }
+    }
+}
+
+/// test insertionSort
 var array = [2, 3, 5, 1]
 insertionSort(array: &array)
 print(array)
 
-//: [Next](@next)
+// Test insertionSort1
+var array1 = [2, 3, 5, 1, -2]
+insertionSort1(array: &array1)
+print(array1)
